54694100ee863d3558f403689274517803458c72
Yimin Zheng <yzheng@cemm.at>
2024-09-26 12:31:18 +0200

    Update docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from functools import cached_property
 
+from patchworklib import inset
 from sphinxawesome_theme.postprocess import Icons
 
 import wsidata
@@ -14,7 +16,6 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.autosectionlabel",
-    "matplotlib.sphinxext.plot_directive",
     "sphinx.ext.intersphinx",
     "sphinx_design",
     "sphinx_copybutton",
@@ -31,6 +32,7 @@ exclude_patterns = []
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
+html_logo = "_static/logo.svg"
 html_css_files = ["custom.css"]
 html_permalinks_icon = Icons.permalinks_icon
 html_theme_options = {
@@ -40,7 +42,38 @@ html_theme_options = {
 
 html_sidebars = {"installation": [], "intro": []}
 
-jupyter_execute_notebooks = "off"
+nb_execution_mode = "cache"
 
 copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5," r"8}: "
 copybutton_prompt_is_regexp = True
+
+intersphinx_mapping = {
+    "spatialdata": ("https://spatialdata.scverse.org/en/latest", None),
+    "shapely": ("https://shapely.readthedocs.io/en/stable", None),
+    "xarray": ("https://docs.xarray.dev/en/stable/", None),
+    "geopandas": ("https://geopandas.org/en/stable/", None),
+    "anndata": ("https://anndata.readthedocs.io/en/latest/", None),
+}
+
+
+def autodoc_skip_member(app, what, name, obj, skip, options):
+    exclude_wsidata_attrs = {
+        "SLIDE_PROPERTIES_KEY",
+        "TILE_SPEC_KEY",
+    }
+
+    if isinstance(obj, property):
+        return True
+    elif isinstance(obj, cached_property):
+        return True
+
+    if hasattr(obj, "__qualname__"):
+        cls = obj.__qualname__.split(".")[0]
+        if (cls == "WSIData") & (name in exclude_wsidata_attrs):
+            return True
+        if cls == "SlideProperties":
+            print(what, obj, name)
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", autodoc_skip_member)
