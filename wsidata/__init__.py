3d8372376001852ce16ddc300a3627ebf484d236
Yimin Zheng <yzheng@cemm.at>
2024-12-12 12:45:16 +0100

    Bump version

--- wsidata/__init__.py
+++ wsidata/__init__.py
@@ -1,6 +1,6 @@
 """Data structures and I/O functions for whole-slide images (WSIs)."""
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 import wsidata.dataset as dataset
 from ._model import WSIData, TileSpec
