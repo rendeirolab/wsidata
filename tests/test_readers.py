781614ab8bec5823d43e262bafb78bf7044603ff
Yimin Zheng <yzheng@cemm.at>
2025-11-01 10:04:09 +0100

    Update API and tests

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -27,7 +27,7 @@ def run_reader_test(reader, test_slide):
     wsi.associated_images
     wsi.thumbnail
     wsi.get_thumbnail(as_array=True)
-    assert wsi.reader.translate_level(-1) == 0
+    assert wsi.reader.translate_level(-1) == wsi.properties.n_level - 1
 
 
 @pytest.mark.skipif(skip_reader("openslide"), reason="openslide not installed")
@@ -51,3 +51,27 @@ def test_bioformats(test_slide):
 @pytest.mark.skipif(skip_reader("cucim"), reason="cucim not installed")
 def test_cucim(test_slide):
     run_reader_test("cucim", test_slide)
+
+
+def test_spatialdata(test_slide):
+    import numpy as np
+    from spatialdata import SpatialData
+    from spatialdata.models import Image2DModel
+
+    img = np.random.randint(0, 256, (3, 512, 512), dtype=np.uint8)
+    images = Image2DModel.parse(img, dims=("c", "y", "x"))
+
+    big_img = np.random.randint(0, 256, (3, 5120, 5120), dtype=np.uint8)
+    ms_images = Image2DModel.parse(big_img, dims=("c", "y", "x"), scale_factors=[2, 2])
+
+    sdata = SpatialData(images={"img": images, "ms_img": ms_images})
+
+    wsi = open_wsi(sdata, image_key="img")
+    wsi.read_region(0, 0, 10, 10, level=0)
+    wsi.get_thumbnail(as_array=True)
+
+    wsi = open_wsi(sdata, image_key="ms_img")
+    wsi.read_region(0, 0, 10, 10, level=0)
+    wsi.get_thumbnail(as_array=True)
+
+    assert wsi.reader.translate_level(-1) == wsi.properties.n_level - 1
