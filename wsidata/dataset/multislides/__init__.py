e026e07fcae5be56f12eb753a88cf3dbba6f3c6e
Yimin Zheng <yzheng@cemm.at>
2026-02-06 12:45:32 +0100

    Remove ignoring F401 and reformat

--- wsidata/dataset/multislides/__init__.py
+++ wsidata/dataset/multislides/__init__.py
@@ -1,2 +1,9 @@
 from .builder import FeaturesDatasetBuilder
 from .sampler import InSlideUnderSampler, NoBalance, RandomUnderSampler
+
+__all__ = [
+    "FeaturesDatasetBuilder",
+    "InSlideUnderSampler",
+    "NoBalance",
+    "RandomUnderSampler",
+]
--- wsidata/io/__init__.py
+++ wsidata/io/__init__.py
@@ -10,3 +10,18 @@ from ._elems import (
     update_shapes_data,
 )
 from ._wsi import agg_wsi, concat_feature_anndata, open_wsi
+
+__all__ = [
+    "add_tiles",
+    "add_table",
+    "add_shapes",
+    "add_tissues",
+    "add_features",
+    "agg_wsi",
+    "add_agg_features",
+    "concat_feature_anndata",
+    "open_wsi",
+    "subset_tiles",
+    "sync_tile_spec",
+    "update_shapes_data",
+]
